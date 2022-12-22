d50fae3c5b8ce3278632649008a43d62f89f23e6
dzikrikhairizal46 <dkhairizal1711@gmail.com>
2022-12-22 04:48:10 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,71 @@
+$(document).ready(function() {
+
+	/***************** Waypoints ******************/
+
+	$('.wp1').waypoint(function() {
+		$('.wp1').addClass('animated fadeInLeft');
+	}, {
+		offset: '75%'
+	});
+	$('.wp2').waypoint(function() {
+		$('.wp2').addClass('animated fadeInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp3').waypoint(function() {
+		$('.wp3').addClass('animated bounceInDown');
+	}, {
+		offset: '75%'
+	});
+	$('.wp4').waypoint(function() {
+		$('.wp4').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp5').waypoint(function() {
+		$('.wp5').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp6').waypoint(function() {
+		$('.wp6').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+	$('.wp7').waypoint(function() {
+		$('.wp7').addClass('animated fadeInUp');
+	}, {
+		offset: '75%'
+	});
+
+	/***************** Flickity ******************/
+
+	$('#featuresSlider').flickity({
+		cellAlign: 'left',
+		contain: true,
+		prevNextButtons: false
+	});
+
+	$('#showcaseSlider').flickity({
+		cellAlign: 'left',
+		contain: true,
+		prevNextButtons: false,
+		imagesLoaded: true
+	});
+});
+
+/***************** Smooth Scrolling ******************/
+
+$('a[href*=#]:not([href=#])').click(function() {
+	if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: target.offset().top
+			}, 2000);
+			return false;
+		}
+	}
+});
